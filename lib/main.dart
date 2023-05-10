2a200162da12649ca134b837fd15bbe0da2a3cc2
Thiago Cesar <thiagocesarmata@gmail.com>
2023-05-10 11:57:08 -0300

    feat: remove hash (#) from url web

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'src/app_widget.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
